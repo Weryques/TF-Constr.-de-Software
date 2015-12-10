83ac825ed10a3ea5c3c66a158b96cc316334edec
weryquessantos <weryquessantos@192.168.100.3>
2015-12-10 17:28:47 -0200

    visao

---
+++ mercadoFacil/src/main/java/mercadofacil/controlador/ServletConexaoBD.java
@@ -0,0 +1,11 @@
+package mercadofacil.controlador;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @author weryquessantos
+ *
+ */
+public class ServletConexaoBD extends HttpServlet{
+
+}
---
+++ mercadoFacil/src/main/java/mercadofacil/controlador/ServletDirecionamentoPagina.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package mercadofacil.controlador;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @author weryquessantos
+ *
+ */
+public class ServletDirecionamentoPagina extends HttpServlet {
+
+}
