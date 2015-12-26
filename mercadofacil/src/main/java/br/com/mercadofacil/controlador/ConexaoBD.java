015dbe4dc6db199580c2222376e1d967913c3095
weryquessantos <weryquessantos@192.168.100.4>
2015-12-26 14:58:18 -0200

    bd

--- mercadofacil/src/main/java/br/com/mercadofacil/controlador/ConexaoBD.java
+++ mercadofacil/src/main/java/br/com/mercadofacil/controlador/ConexaoBD.java
@@ -1,19 +1,45 @@
 package br.com.mercadofacil.controlador;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 
 /**
  * @author weryquessantos
  *
  */
-public class ConexaoBD{
+@WebServlet(value = "/conexao")
+public class ConexaoBD extends HttpServlet{
 	
 	private Connection conexao;
 	
-	private Connection conectar() throws SQLException{
-		String url = "jdbc:mysql://localhost/mercadofacil";
-		return conexao = DriverManager.getConnection(url);
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		conectar();
+	}
+	
+	private void conectar(){
+		String url = "jdbc:mysql://localhost:3306/";
+		String nomeBD = "mercadofacil";
+		String usuario = "root";
+		String senha = "";
+		
+		try {
+			conexao = DriverManager.getConnection(url + nomeBD, usuario, senha);
+			System.out.println("A conexão com o banco de dados ocorreu com sucesso!");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage() +"\n"+ e.getSQLState());
+	
+		}
 	}
 }
---
+++ mercadofacil/src/main/java/br/com/mercadofacil/controlador/UpdateBD.java
@@ -0,0 +1,12 @@
+package br.com.mercadofacil.controlador;
+
+/**
+ * @author weryquessantos
+ * Classe com metodos de atualizacoes no bando de dados - insert, update, delete.
+ */
+public class UpdateBD{
+		
+	private void inserirConsumidor(String sql){
+		
+	}
+}
