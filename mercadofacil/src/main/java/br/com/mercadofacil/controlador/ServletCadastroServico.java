11e2996b6c99f49b9cbae0c270d83f331d6292d3
weryquessantos <weryquessantos@192.168.100.6>
2016-01-05 15:51:42 -0200

    Cadastro de serviço

--- mercadofacil/src/main/java/br/com/mercadofacil/controlador/ServletCadastroServico.java
+++ mercadofacil/src/main/java/br/com/mercadofacil/controlador/ServletCadastroServico.java
@@ -2,6 +2,8 @@ package br.com.mercadofacil.controlador;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -10,6 +12,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
 import br.com.mercadofacil.jdbc.ConexaoBD;
+import br.com.mercadofacil.jdbc.UpdateBD;
 import br.com.mercadofacil.modelo.Servico;
 
 @WebServlet(value = "/cadastrarservico")
@@ -26,7 +29,28 @@ public class ServletCadastroServico extends HttpServlet{
 			
 			conexao = conn.conectar();
 			
+			servico.setNome(req.getParameter("nome"));
+			servico.setDescricao(req.getParameter("descricao"));
+			servico.setCpnj(req.getParameter("cnpj"));
+			servico.setEmail(req.getParameter("email"));
+			servico.setRazaoAnunciante(req.getParameter("razaoSocial"));
 			
+			UpdateBD update = new UpdateBD();
+			Statement stmt = null;
+			
+			//validar informações antes de gravar.
+			
+			try {
+				stmt = conexao.createStatement();
+				
+				stmt.executeQuery(update.inserirServico(servico.getNome(), servico.getDescricao(), 
+						servico.getCpnj(), servico.getEmail(), servico.getRazaoAnunciante()));
+				
+				stmt.close();
+				conexao.close();
+			} catch (SQLException e) {
+				System.out.println("Erro em cadastrar serviço: "+ e.getMessage() +"\nCodigo do erro: "+ e.getErrorCode());
+			}
 		}
 		else{
 			req.getRequestDispatcher("visao/alguma página").forward(req, res);
