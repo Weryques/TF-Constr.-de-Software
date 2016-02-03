bbb79ba0bb7c5fb2b85392e574893df3682ba8b5
weryquessantos <weryquessantos@192.168.100.4>
2016-02-03 00:18:03 -0200

    Servlet

---
+++ mercadofacil/src/main/java/br/com/mercadofacil/controlador/ServletDeletaUsuario.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package br.com.mercadofacil.controlador;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @author weryquessantos
+ *
+ */
+@WebServlet (value = "/deletaconta")
+public class ServletDeletaUsuario extends HttpServlet{
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		
+	}
+}
